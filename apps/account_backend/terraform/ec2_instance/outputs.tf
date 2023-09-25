1c8d5a22bfa1ae985220de08d19ac8f5d31349d7
Justin Hagerty <wheelsmanx@gmail.com>
2023-09-25 13:13:34 -0400

    adjusted tf

--- apps/account_backend/terraform/ec2_instance/outputs.tf
+++ apps/account_backend/terraform/ec2_instance/outputs.tf
@@ -5,6 +5,6 @@ output "instance_id" {
 
 output "backend_security_group_id" {
   description = "The ID of the security group associated with the backend EC2 instance."
-  value       = length(data.aws_security_group.existing_backend_sg) > 0 ? data.aws_security_group.existing_backend_sg[0].id : aws_security_group.backend.id
+  value       = length(data.aws_security_group.existing) > 0 ? data.aws_security_group.existing[0].id : aws_security_group.backend.id
 }
 
