87446c8880bd4f0ac817e00aae896d60b0243618
Justin Hagerty <wheelsmanx@gmail.com>
2023-09-25 12:28:09 -0400

    adjust tf

--- apps/account_backend/terraform/ec2_instance/variables.tf
+++ apps/account_backend/terraform/ec2_instance/variables.tf
@@ -1,13 +1,3 @@
-variable "module_ami_id" {
-  description = "The ID of the AMI to be used"
-  type        = string
-}
-
-variable "module_instance_type" {
-  description = "The instance type of the EC2 instance"
-  type        = string
-}
-
 variable "subnet_id" {
   description = "The ID of the subnet where the instance should be deployed"
   type        = string
