4244fa43ef1d4abc61590289094e5dfcb20276d2
Justin Hagerty <wheelsmanx@gmail.com>
2023-09-26 18:05:31 -0400

    added db_url output

--- apps/account_backend/terraform/mysql_db/outputs.tf
+++ apps/account_backend/terraform/mysql_db/outputs.tf
@@ -1,3 +1,8 @@
 output "db_endpoint" {
   value = aws_db_instance.default.endpoint
 }
+
+output "db_url" {
+  description = "Database URL without the port."
+  value      = split(":", self.db_endpoint)[0]
+}
